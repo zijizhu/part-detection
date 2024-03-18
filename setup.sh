db8e9a7ef5bb14f7a74f47f77c4d235b05d6e71c
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-18 11:08:06 +1100

    setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+python -m pip install -U torch torchvision torchaudio jupyterlab matplotlib pandas scipy scikit-learn scikit-image
+
+mkdir datasets/cub
+cd datasets/cub
+wget -q --show-progress "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
+tar -xzf CUB_200_2011.tgz
